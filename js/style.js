eca642a78ff0009e28a181b972285e953b3b3b27
ldudkowski3 <lance.dudkowski@gmail.com>
2020-09-20 00:04:57 -0400

    last changes

--- js/style.js
+++ js/style.js
@@ -41,3 +41,9 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+$(".navHover").hover(function(){
+  $(this).css("color", "black");
+  }, function(){
+  $(this).css("color", "#505050");
+});
